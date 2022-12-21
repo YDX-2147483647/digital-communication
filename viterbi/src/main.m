5a95ca33245ce74269e748cd61519964a129d4b1
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2022-12-21 20:25:28 +0800

    [viterbi] 展示译码算法

--- viterbi/src/main.m
+++ viterbi/src/main.m
@@ -6,4 +6,13 @@ fprintf("- 信源序列 b:    %s.\n", join(string(b), ",    "));
 
 g = [1 1 1; 1 0 1];
 c = conv_encode(b, g);
-fprintf("- 编码序列 c: %s.\n\n", join(string(c), ", "));
+fprintf("- 编码序列 c: %s.\n", join(string(c), ", "));
+
+d = conv_decode(c, g);
+fprintf("- 译码序列 d:    %s.\n\n", join(string(d), ",    "));
+
+if isequal(d, b)
+    fprintf("✓ 译码全部正确。\n")
+else
+    fprintf("✗ 译码错了 %d 位。\n", sum(abs(d - b)));
+end
